4d9d05c5487d6408708450f62d60167f39535a41
rebecaborges <rebeca.felix@hotmail.com>
2019-01-31 20:22:17 -0200

    adicionei css

--- cipher.js
+++ cipher.js
@@ -1,16 +1,16 @@
 function buttonEncode(){
     let str = document.getElementById('text1').value
-    let offPosition = parseInt(document.getElementById('desloc').value)
+    let offPosition = document.getElementById('desloc').value
     let outEncode = encode(str,offPosition)
-    document.getElementById('textCifra').innerHTML= outEncode
-
+    document.getElementById('textCifra').innerHTML= 'Sua mensagem cifrada é: '+outEncode;
+    
 }
 
 function buttonDecode(){
     let str = document.getElementById('text1').value
     let offPosition = document.getElementById('desloc').value
     let outDecode = decode(str,offPosition)
-    document.getElementById('textDescifra').innerHTML= outDecode
+    document.getElementById('textDescifra').innerHTML= 'Sua mensagem decifrada é: '+outDecode
 }
 
 function encode (str,offPosition){
@@ -19,11 +19,11 @@ function encode (str,offPosition){
 
     for (let i=0; i < str.length; i++){
         if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
-        ascii = ((str.charCodeAt(i) -65 + offPosition)% 26)+65;
+        ascii = ((str.charCodeAt(i) -65+(offPosition%26)+26)%26)+65;
         outText += String.fromCharCode(ascii)
         
         }else if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
-        ascii = ((str.charCodeAt(i) -97 + offPosition)% 26)+97;
+        ascii = ((str.charCodeAt(i) -97+(offPosition%26)+26)%26)+97;
         outText += String.fromCharCode(ascii)
         
         }else { ascii =str.charCodeAt(i)
