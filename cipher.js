e8139db64cec0f6fa8c746abb0a795145858e7f1
rebecaborges <rebeca.felix@hotmail.com>
2019-02-01 15:21:59 -0200

    finalizei css e alterei funcoes

--- cipher.js
+++ cipher.js
@@ -1,6 +1,6 @@
 function buttonEncode(){
     let str = document.getElementById('text1').value
-    let offPosition = document.getElementById('desloc').value
+    let offPosition = parseInt(document.getElementById('desloc').value)
     let outEncode = encode(str,offPosition)
     document.getElementById('textCifra').innerHTML= 'Sua mensagem cifrada é: '+outEncode;
     
@@ -8,22 +8,22 @@ function buttonEncode(){
 
 function buttonDecode(){
     let str = document.getElementById('text1').value
-    let offPosition = document.getElementById('desloc').value
+    let offPosition = parseInt(document.getElementById('desloc').value)
     let outDecode = decode(str,offPosition)
     document.getElementById('textDescifra').innerHTML= 'Sua mensagem decifrada é: '+outDecode
 }
 
 function encode (str,offPosition){
-    let ascii;
-    let outText ='';
+    let ascii; // guarda a formula de deslocamento
+    let outText =''; // guarda a posição da string com deslocamento, uma ao lado da outra, formando uma string completa
 
     for (let i=0; i < str.length; i++){
         if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
-        ascii = ((str.charCodeAt(i) -65+(offPosition%26)+26)%26)+65;
-        outText += String.fromCharCode(ascii)
+        ascii = ((str.charCodeAt(i) -65 + offPosition)%26)+65;
+        outText += String.fromCharCode(ascii) //pega a posição da string e concatena com a soma do deslocamento
         
         }else if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
-        ascii = ((str.charCodeAt(i) -97+(offPosition%26)+26)%26)+97;
+        ascii = ((str.charCodeAt(i) -97 + offPosition)%26)+97;
         outText += String.fromCharCode(ascii)
         
         }else { ascii =str.charCodeAt(i)
@@ -35,16 +35,16 @@ function encode (str,offPosition){
 }
 
 function decode(str,offPosition){
-    let ascii;
-    let outText= "";
+    let ascii; // guarda a formula de deslocamento
+    let outText= ""; 
   
     for(i=0; i<str.length; i++){
         if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
-        ascii = ((str.charCodeAt(i) -65-(offPosition%26)+26)%26)+65;
+        ascii = (str.charCodeAt(i) -65- (offPosition)%26)+65;
         outText += String.fromCharCode(ascii)
   
         }else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
-        ascii = ((str.charCodeAt(i) -97-(offPosition%26)+26)%26)+97;
+        ascii = (str.charCodeAt(i) -97- (offPosition)%26)+97;
         outText += String.fromCharCode(ascii)
 
         }else {ascii = str.charCodeAt(i)
@@ -52,4 +52,4 @@ function decode(str,offPosition){
         }    
     }return outText
 
-}
+} 
--- tests.js
+++ tests.js
@@ -3,14 +3,12 @@ function tests1(){
     if (testEncode === 'bcd'){
         console.log('deu certo')
     }else{console.log('deu errado, o resultado foi: '+testEncode)
-
     }   
 }
 
-
 function tests2(){
-    let testDecode = decode('abc',1);
-    if (testDecode === 'bcd'){
+    let testDecode = decode('bcd',1);
+    if (testDecode === 'abc'){
         console.log('deu certo')
     }else{'deu errado, o resultado foi :'+testDecode
     }
